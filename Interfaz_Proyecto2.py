32f5a08b0610abdb4514113278bd5cc7a6fe2c1d
Jorge Lorenzana <lor17302@uvg.edu.gt>
2018-11-23 08:27:00 -0600

    Interfaz con Lectura

--- Interfaz_Proyecto2.py
+++ Interfaz_Proyecto2.py
@@ -1,5 +1,6 @@
 from tkinter import *
 import serial, time
+from tkinter import messagebox
 
 ##while(1):
 ##    while(1):
@@ -40,24 +41,101 @@ Rutina = []
 ##while(Vin is None):
 ##    Vin = Pic.readline(1)
 ##Vin = ord(Vin)
-lectura = False
+lectura1 = False
+lectura2 = False
+lectura3 = False
 
-def serialR():
-    global lectura
-    if(lectura):
-        lectura = False
-        print("False")
+
+def Leer1():
+    global lectura1
+    if(lectura1):
+        lectura1 = False
+        G1.config(image=BG1)
+        G2.config(command=Leer2)
+        G3.config(command=Leer3)
     else:
-        lectura = True
-        print("True")
+        lectura1 = True
+        G1.config(image=Stop)
+        G2.config(command=Error)
+        G3.config(command=Error)
         
-    while(lectura):
-        print("Leyendo")
+    while(lectura1):
+        print("Leyendo1")
         Main.update()
     return
 
+def Leer2():
+    global lectura2
+    if(lectura2):
+        lectura2 = False
+        G2.config(image=BG2)
+        G1.config(command=Leer1)
+        G3.config(command=Leer3)
+    else:
+        lectura2 = True
+        G2.config(image=Stop)
+        G1.config(command=Error)
+        G3.config(command=Error)
+        
+    while(lectura2):
+        print("Leyendo2")
+        Main.update()
+    return
 
-button = Button(Main, command = serialR).pack()
+def Leer3():
+    global lectura3
+    if(lectura3):
+        lectura3 = False
+        G3.config(image=BG3)
+        G1.config(command=Leer1)
+        G2.config(command=Leer2)
+    else:
+        lectura3 = True
+        G3.config(image=Stop)
+        G1.config(command=Error)
+        G2.config(command=Error)
+        
+    while(lectura3):
+        print("Leyendo3")
+        Main.update()
+    return
+
+def Enviar1():
+    return
+
+def Enviar2():
+    return
+
+def Enviar3():
+    return
+
+def Error():
+    messagebox.showerror("Error", "Hay una rutina grabando")
+    return
+
+Stop = PhotoImage(file = "Stop.PNG")
+
+BG1 = PhotoImage(file="Boton_G1.PNG")
+BG2 = PhotoImage(file="Boton_G2.PNG")
+BG3 = PhotoImage(file="Boton_G3.PNG")
+
+G1 = Button(Main, command = Leer1, image=BG1, bg = "Black", border = 0)
+G1.place(relx=0.2, rely=0.3)
+G2 = Button(Main, command = Leer2, image=BG2, bg = "Black", border = 0)
+G2.place(relx=0.1, rely=0.45)
+G3 = Button(Main, command = Leer3, image=BG3, bg = "Black", border = 0)
+G3.place(relx=0.3, rely=0.45)
+
+BR1 = PhotoImage(file="Boton_R1.PNG")
+BR2 = PhotoImage(file="Boton_R2.PNG")
+BR3 = PhotoImage(file="Boton_R3.PNG")
+
+R1 = Button(Main, command = Enviar1, image=BR1, bg = "Black", border = 0)
+R1.place(relx=0.7, rely=0.3)
+R2 = Button(Main, command = Enviar2, image=BR2, bg = "Black", border = 0)
+R2.place(relx=0.6, rely=0.45)
+R3 = Button(Main, command = Enviar3, image=BR3, bg = "Black", border = 0)
+R3.place(relx=0.8, rely=0.45)
 
 
 while True:
